e091f238a4b9ad7843c9b223fcd0308e9468f56d
Po Chen <chenpaul914@gmail.com>
2021-02-10 15:29:49 +1100

    allow users to pick an adapter

---
+++ config/config.exs
@@ -0,0 +1,3 @@
+import Config
+
+config :tesla, Cloudflare.Client, adapter: Tesla.Adapter.Mint
--- lib/cloudflare/client.ex
+++ lib/cloudflare/client.ex
@@ -1,5 +1,4 @@
 defmodule Cloudflare.Client do
   use Restlax.Client,
-    adapter: Tesla.Adapter.Mint,
     base_url: "https://api.cloudflare.com/client/v4"
 end
